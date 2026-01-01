e6e13f2ee8166fb973e4e74cc1aadd3899578085
vamsithabonda <155445021+vamsithabonda@users.noreply.github.com>
2026-01-01 12:30:09 +0530

    Update2 abc.py

--- abc.py
+++ abc.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route("/hello", methods = ['GET'])
+
+@app.route("/hello", methods=['GET'])
 def hello():
-  return "Hello Vamsitha"
-if __name__ == "__main__:
-  app.run(debug=True)
+    return "Hello Vamsitha"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
